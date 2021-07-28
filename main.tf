17c23dff7a2af9f398e0dc52b592004505d26ced
Musa Zwane <musa@obsidian.co.za>
2021-07-29 01:29:22 +0200

    Added a new main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,17 @@
-###Test
+provider "aws" {
+    region = "us-east-2"
+}
+
+
+resource "aws_vpc" "testVPC1" {
+
+cidr_block = "10.0.0.0/16"
+
+}
+
+resource "aws_instance" "Kubernetes1" {
+
+instance_type = "t2.micro"
+ami = "ami-02b0c55eeae6d5096"
+
+}
